5253825c4e458080fa5aa2258ee7312a58ce266b
tahir-ali <tahir.ali@feinternational.com>
2023-10-05 14:27:01 +0500

    login and register functionality

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,90 @@
+import { useContext, useState } from "react";
+import AuthContext from "../store/AuthContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+  const authCtx = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    axios({
+      url: "http://localhost:5000/api/v1/auth/register",
+      method: "POST",
+      data: { username, password, email },
+    })
+      .then((res) => {
+        //window.localStorage.setItem("isAuthenticated", true);
+        console.log(res);
+        if (res.status === 200) {
+          if (res.data.error) {
+            setError(res.data.error);
+          } else {
+            const user = {
+              name: res.data.username,
+              id: res.data._id,
+            };
+            authCtx.login(user);
+            navigate("/restaurants");
+          }
+        }
+      })
+      .catch(({ response }) => {
+        console.log(response);
+        setError(response.data.message || response.data);
+      });
+  };
+
+  return (
+    <form
+      action="http://localhost:5000/api/v1/auth/login"
+      onSubmit={loginHandler}
+      method="POST"
+      noValidate
+      className="validated-form"
+    >
+      <div>
+        {error}
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button className="btn btn-success mt-1" type="submit">
+          Register
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Register;
---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,7 @@
+import http from "../http-common";
+
+class AuthService {
+  // login(data) {
+  //   return http.post(`auth/login`, data);
+  // }
+}
